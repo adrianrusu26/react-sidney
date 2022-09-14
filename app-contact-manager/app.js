d688a42a939138e3dde2b7aed3e7386faf061fd9
adrianrusu26 <adrian.rusu176@gmail.com>
2022-09-14 22:14:55 +0300

    curs 14.09.2022

---
+++ app-contact-manager/app.js
@@ -0,0 +1,3 @@
+// browser based modules REQUIRE extention -js
+
+import searchForm from './modules/searchForm.js';
---
+++ app-contact-manager/modules/data.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    name: 'Carol',
+    surname: 'Carolson',
+    phone: '074100000',
+    email: 'carol@carol.ro',
+  },
+  {
+    id: 2,
+    name: 'Larry',
+    surname: 'Larryson',
+    phone: '074110000',
+    email: 'wtv@larry.ro',
+  },
+];
